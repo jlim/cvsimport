94bd10b3ea88583dc1692f389263e3c87cb3e2cf
j_lim <j_lim>
2006-04-06 22:17:19 +0000

    alphabetical tf browsing

---
+++ pazar.info/cgi-bin/tfbrowse_alpha.pl
@@ -0,0 +1,54 @@
+#!/usr/local/bin/perl
+print "Content-type:text/html\n\n";
+
+print<<Page_Done;
+<html>
+<head>
+<title>Browse Transcription Factors Alphabetically</title>
+</head>
+<body bgcolor="#FFFFFF">
+
+<table>
+<tr><td>Browse Transcription Factors Alphabetically: <br>
+<B>
+<!--
+<a href="tfbrowse_result.pl?search_alpha=0">0</a> 
+<a href="tfbrowse_result.pl?search_alpha=1">1</a> 
+<a href="tfbrowse_result.pl?search_alpha=2">2</a> 
+<a href="tfbrowse_result.pl?search_alpha=3">3</a> 
+<a href="tfbrowse_result.pl?search_alpha=4">4</a> 
+<a href="tfbrowse_result.pl?search_alpha=5">5</a> 
+<a href="tfbrowse_result.pl?search_alpha=6">6</a> 
+<a href="tfbrowse_result.pl?search_alpha=7">7</a> 
+<a href="tfbrowse_result.pl?search_alpha=8">8</a> 
+<a href="tfbrowse_result.pl?search_alpha=9">9</a> -->
+<a href="tfbrowse_result.pl?search_alpha=A">A</a> 
+<a href="tfbrowse_result.pl?search_alpha=B">B</a> 
+<a href="tfbrowse_result.pl?search_alpha=C">C</a>
+<a href="tfbrowse_result.pl?search_alpha=D">D</a>
+<a href="tfbrowse_result.pl?search_alpha=E">E</a>
+<a href="tfbrowse_result.pl?search_alpha=F">F</a>
+<a href="tfbrowse_result.pl?search_alpha=G">G</a>
+<a href="tfbrowse_result.pl?search_alpha=H">H</a>
+<a href="tfbrowse_result.pl?search_alpha=I">I</a>
+<a href="tfbrowse_result.pl?search_alpha=J">J</a>
+<a href="tfbrowse_result.pl?search_alpha=K">K</a>
+<a href="tfbrowse_result.pl?search_alpha=L">L</a>
+<a href="tfbrowse_result.pl?search_alpha=M">M</a>
+<a href="tfbrowse_result.pl?search_alpha=N">N</a>
+<a href="tfbrowse_result.pl?search_alpha=O">O</a>
+<a href="tfbrowse_result.pl?search_alpha=P">P</a>
+<a href="tfbrowse_result.pl?search_alpha=Q">Q</a>
+<a href="tfbrowse_result.pl?search_alpha=R">R</a>
+<a href="tfbrowse_result.pl?search_alpha=S">S</a>
+<a href="tfbrowse_result.pl?search_alpha=T">T</a>
+<a href="tfbrowse_result.pl?search_alpha=U">U</a>
+<a href="tfbrowse_result.pl?search_alpha=V">V</a>
+<a href="tfbrowse_result.pl?search_alpha=W">W</a>
+<a href="tfbrowse_result.pl?search_alpha=X">X</a>
+<a href="tfbrowse_result.pl?search_alpha=Y">Y</a>
+<a href="tfbrowse_result.pl?search_alpha=Z">Z</a></B></td></tr>
+</table>
+</body>
+</html>
+Page_Done
