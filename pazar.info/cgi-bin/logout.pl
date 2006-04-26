6e3c56b6d44edb53591dd22da92abdd8476c1cb8
j_lim <j_lim>
2006-04-26 00:23:21 +0000

    cleaned out weird comments

--- pazar.info/cgi-bin/logout.pl
+++ pazar.info/cgi-bin/logout.pl
@@ -1,16 +1,4 @@
-#!/usr/bin/perl
-#
-#  PROGRAM:	cookie-get.cgi
-#
-#  PURPOSE:	Demonstrate how to GET a cookie through a Perl/CGI program
-#		using the CGI.pm module.
-#
-#  Copyright DevDaily Interactive, Inc., 1998. All Rights Reserved.
-#
-
-#------------------------------#
-#  1. Create a new CGI object  #
-#------------------------------#
+#!/usr/local/bin/perl
 
 use CGI;
 use CGI::Cookie;
@@ -29,25 +17,13 @@ $pazarProjectCookie->expires('now');
 #--------------------------------------------------------------#
 
 print $query->header(-cookie=>[$pazarCookie, $pazarProjectCookie]);
-#print $query->header(-cookie=>$pazarCookie);
-#----------------------------------------------------#
-#  3. Start the HTML doc, and give the page a title  #
-#----------------------------------------------------#
-#print $query->start_html;
-# open the html header template
+
 my $template = HTML::Template->new(filename => 'header.tmpl');
 
 # fill in template parameters
 $template->param(TITLE => 'PAZAR Login');
 
 print $template->output;
-
-#----------------------------------------------------------------------#
-#  4. Retrieve the cookie. Do this by using the cookie method without  #
-#     the -value parameter.                                            #
-#----------------------------------------------------------------------#
-
-
 print $query->h3('You are now logged out');
 
 
@@ -56,4 +32,3 @@ print $query->h3('You are now logged out');
 #-------------------------#
 my $template_tail = HTML::Template->new(filename => 'tail.tmpl');
 print $template_tail->output;
-#print $query->end_html;
