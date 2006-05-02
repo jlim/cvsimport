d6947013f9437962b2d044edb4cba0fdcc804f8e
j_lim <j_lim>
2006-05-02 22:19:21 +0000

    remove session from server and cookie from browser

--- pazar.info/cgi-bin/logout.pl
+++ pazar.info/cgi-bin/logout.pl
@@ -2,30 +2,38 @@
 
 use CGI;
 use CGI::Cookie;
+use CGI::Session;
 use HTML::Template;
 
 $query = new CGI;
 
 %cookies = fetch CGI::Cookie;
 $pazarCookie = $cookies{'PAZAR_COOKIE'};
-$pazarProjectCookie = $cookies{'PAZAR_PROJECT_COOKIE'};
-$pazarCookie->expires('now');
-$pazarProjectCookie->expires('now');
+
+$pazarCookie->expires('-1d');
+
+#remove session from server
+$sessionid = $pazarCookie->value;
+
+CGI::Session->name("PAZAR_COOKIE");
+my $session = new CGI::Session();
+
+if($session)
+{
+    $session->delete();
+}
 
 #--------------------------------------------------------------#
 #  2. Create the HTTP header and print the doctype statement.  #
 #--------------------------------------------------------------#
 
-print $query->header(-cookie=>[$pazarCookie, $pazarProjectCookie]);
-
+print $query->header(-cookie=>$pazarCookie);
 my $template = HTML::Template->new(filename => 'header.tmpl');
-
 # fill in template parameters
-$template->param(TITLE => 'PAZAR Login');
+$template->param(TITLE => 'PAZAR Logout');
 
 print $template->output;
 print $query->h3('You are now logged out');
-
 
 #-------------------------#
 #  5. End the HTML page.  #
