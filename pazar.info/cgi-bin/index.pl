e623e48a1c81a7cce71cf09f42aaeee25d44fed6
elodiepc <elodiepc>
2006-08-29 16:41:39 +0000

    absolute link for the login page

--- pazar.info/cgi-bin/index.pl
+++ pazar.info/cgi-bin/index.pl
@@ -21,12 +21,12 @@ $template->param(TITLE => 'PAZAR Mall');
 if($loggedin eq 'true')
 {
     #log out link
-    $template->param(LOGOUT => "$info{first} $info{last} logged in. ".'<a href=\'logout.pl\'>Log Out</a>');
+    $template->param(LOGOUT => '<a href=\'logout.pl\'>Log Out</a>');
 }
 else
 {
     #log in link
-    $template->param(LOGOUT => '<a href=\'login.pl\'>Log In</a>');
+    $template->param(LOGOUT => '<a href=\'http://www.pazar.info/cgi-bin/login.pl\'>Log In</a>');
 }
 
 # send the obligatory Content-Type and print the template output
@@ -41,66 +41,25 @@ my $dbh = pazar->new(
 		      -drv           =>    DB_DRV,
                       -globalsearch  =>    'yes');
 
-my $projects=&select($dbh, "SELECT * FROM project WHERE upper(status)='OPEN' OR upper(status)='PUBLISHED'");
+my $projects=&select($dbh, "SELECT * FROM project WHERE status='open' OR status='published'");
 my @desc;
 while (my $project=$projects->fetchrow_hashref) {
     push @desc, {
-	name => $project->{project_name},
-        description => $project->{description}};
+	name => $project->{project_name}};
+#       description => $project->{description}}
+###Add description when the schema is changed
 }
-
-if ($loggedin eq 'true') {
-    foreach my $proj (@projids) {
-	my $restricted=&select($dbh, "SELECT * FROM project WHERE project_id='$proj' and upper(status)='RESTRICTED'");
-	while (my $restr=$restricted->fetchrow_hashref) {
-	    push @desc, {
-		name => $restr->{project_name},
-                description => $project->{description}};
-	}
-    }
-}
-
 undef(my $flashvars);
-my $i=0;
-while ($i<10) {
+for (my $i=0;$i<10;$i++) {
     my $num=sprintf("%02d",($i+1));
     if ($desc[$i]) {
-	my $PTtag='PTM'.$num;
-	my $PTval=$desc[$i]->{name};
-	my $PDtag='PDM'.$num;
-	my $PDval=$desc[$i]->{description}||'No description available';
-	my $PUtag='PUM'.$num;
-	my $PUval='project.pl';
-	if (!$flashvars) {
-	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	} else {
-	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	}
-    } else {
-	my $PTtag='PTM'.$num;
-	my $PTval='For Lease';
-	my $PDtag='PDM'.$num;
-	my $PDval='This space is currently unoccupied. Open your own store in this great location. Rent is free.';
-	my $PUtag='PUM'.$num;
-	my $PUval='register.pl';
-	if (!$flashvars) {
-	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	} else {
-	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	}
-    }
-    $i++;
-}
-my $j=0;
-while (10<=$i&&$i<20) {
-    my $num=sprintf("%02d",($j+1));
-    if ($desc[$i]) {
 	my $PTtag='PT1'.$num;
 	my $PTval=$desc[$i]->{name};
 	my $PDtag='PD1'.$num;
-	my $PDval=$desc[$i]->{description}||'No description available';
+	my $PDval='some description';
+#	my $PDval=$desc[$i]->{description};
 	my $PUtag='PU1'.$num;
-	my $PUval='project.pl';
+	my $PUval='project.pl&name='.$PTval;
 	if (!$flashvars) {
 	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
 	} else {
@@ -119,142 +78,11 @@ while (10<=$i&&$i<20) {
 	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
 	}
     }
-    $i++;
-    $j++;
 }
-$j=0;
-while (20<=$i&&$i<30) {
-    my $num=sprintf("%02d",($j+1));
-    if ($desc[$i]) {
-	my $PTtag='PT2'.$num;
-	my $PTval=$desc[$i]->{name};
-	my $PDtag='PD2'.$num;
-	my $PDval=$desc[$i]->{description}||'No description available';
-	my $PUtag='PU2'.$num;
-	my $PUval='project.pl';
-	if (!$flashvars) {
-	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	} else {
-	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	}
-    } else {
-	my $PTtag='PT2'.$num;
-	my $PTval='For Lease';
-	my $PDtag='PD2'.$num;
-	my $PDval='This space is currently unoccupied. Open your own store in this great location. Rent is free.';
-	my $PUtag='PU2'.$num;
-	my $PUval='register.pl';
-	if (!$flashvars) {
-	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	} else {
-	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	}
-    }
-    $i++;
-    $j++;
-}
-$j=0;
-while (30<=$i&&$i<40) {
-    my $num=sprintf("%02d",($j+1));
-    if ($desc[$i]) {
-	my $PTtag='PT3'.$num;
-	my $PTval=$desc[$i]->{name};
-	my $PDtag='PD3'.$num;
-	my $PDval=$desc[$i]->{description}||'No description available';
-	my $PUtag='PU3'.$num;
-	my $PUval='project.pl';
-	if (!$flashvars) {
-	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	} else {
-	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	}
-    } else {
-	my $PTtag='PT3'.$num;
-	my $PTval='For Lease';
-	my $PDtag='PD3'.$num;
-	my $PDval='This space is currently unoccupied. Open your own store in this great location. Rent is free.';
-	my $PUtag='PU3'.$num;
-	my $PUval='register.pl';
-	if (!$flashvars) {
-	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	} else {
-	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	}
-    }
-    $i++;
-    $j++;
-}
-$j=0;
- while (40<=$i&&$i<50) {
-    my $num=sprintf("%02d",($j+1));
-    if ($desc[$i]) {
-	my $PTtag='PT4'.$num;
-	my $PTval=$desc[$i]->{name};
-	my $PDtag='PD4'.$num;
-	my $PDval=$desc[$i]->{description}||'No description available';
-	my $PUtag='PU4'.$num;
-	my $PUval='project.pl';
-	if (!$flashvars) {
-	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	} else {
-	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	}
-    } else {
-	my $PTtag='PT4'.$num;
-	my $PTval='For Lease';
-	my $PDtag='PD4'.$num;
-	my $PDval='This space is currently unoccupied. Open your own store in this great location. Rent is free.';
-	my $PUtag='PU4'.$num;
-	my $PUval='register.pl';
-	if (!$flashvars) {
-	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	} else {
-	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	}
-    }
-    $i++;
-    $j++;
-}
-$j=0;
-while (50<=$i&&$i<60) {
-    my $num=sprintf("%02d",($j+1));
-    if ($desc[$i]) {
-	my $PTtag='PT5'.$num;
-	my $PTval=$desc[$i]->{name};
-	my $PDtag='PD5'.$num;
-	my $PDval=$desc[$i]->{description}||'No description available';
-	my $PUtag='PU5'.$num;
-	my $PUval='project.pl';
-	if (!$flashvars) {
-	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	} else {
-	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	}
-    } else {
-	my $PTtag='PT5'.$num;
-	my $PTval='For Lease';
-	my $PDtag='PD5'.$num;
-	my $PDval='This space is currently unoccupied. Open your own store in this great location. Rent is free.';
-	my $PUtag='PU5'.$num;
-	my $PUval='register.pl';
-	if (!$flashvars) {
-	    $flashvars=$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	} else {
-	    $flashvars.="&".$PTtag."=".$PTval."&".$PDtag."=".$PDval."&".$PUtag."=".$PUval;
-	}
-    }
-    $i++;
-    $j++;
-}
-
+ 
 
                  
 print<<page;
-<table border="0" cellpadding="0" cellspacing="0" width="550">
-<tbody><tr><td style="text-align:justify"><b>WELCOME TO PAZAR MALL!</b><br>
-PAZAR can be searched by <a href="http://www.pazar.info/cgi-bin/gsearch.pl">Gene</a>, <a href="http://www.pazar.info/cgi-bin/tfsearch.pl">Transcription Factor</a> or <a href="http://www.pazar.info/cgi-bin/profilesearch.pl">Profile</a> by clicking on one of the department stores below.<br>
-Each project in PAZAR is a boutique in the mall. You can limit your search to a specific project by clicking on the corresponding boutique on the mall map.<br>
-If you own restricted projects, log in and they will appear in the mall map. If you just created a project and it does not appear on the mall map, please log out and log in again.</td></tr></tbody></table>
 <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://fpdownload.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=7,0,0,0" width="600" height="700" id="TF_Mall" align="middle">
 <param name="allowScriptAccess" value="sameDomain" />
 page
